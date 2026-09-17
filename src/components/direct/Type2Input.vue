4f41dd2c4aa2984c3431b96e15b9c6789d9d1b05
黄雅雯 <yawen-huang@outlook.com>
2026-09-17 10:04:34 +0800

    指令页面刷新问题修复

--- src/components/direct/Type2Input.vue
+++ src/components/direct/Type2Input.vue
@@ -11,12 +11,14 @@
     size="large"
     style="width:150px"
     @input="val => $emit('update:modelValue', val)" 
-    @change="val => $emit('save', val)"
-  />
+    @change="val => $emit('save', val)" 
+    @keydown.enter="blurOnEnter"
+    />
 </template>
 
 <script setup>
 const props = defineProps({ node: Object, modelValue: [Number, String] })
 // 我们多定义一个 save 事件，用来告诉父组件：该写数据库了
 defineEmits(['update:modelValue', 'save'])
+ const blurOnEnter = (e) => e.target.blur()
 </script>
